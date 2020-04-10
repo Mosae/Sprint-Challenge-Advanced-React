414d2bd9db13bf00c2d1dce428fd7ca3aa1f7c24
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-10 11:20:13 -0500

    created local storage and dark mode component- created button for dark mode

--- client/src/Players.js
+++ client/src/Players.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useDarkMode from './hooks/useDarkMode';
 
 const Players = (props) => {
+	const [darkMode, setDarkMode] = useDarkMode();
+	const toggleMode = (e) => {
+		e.preventDefault();
+		setDarkMode(!darkMode);
+	};
 	return (
 		<div>
 			<h1>Players</h1>
+			<button>Dark Mode</button>
 			<hr />
 			<h3>
 				Names:
--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -1,5 +1,17 @@
 import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 const useDarkMode = (key) => {
-	const [enableDarkMode, setEnableDarkMode] = useLocalStorage(key);
+	const [enableDarkMode, setEnableDarkMode] = useLocalStorage('key');
+
+	useEffect(() => {
+		const className = 'dark-mode';
+		const bodyElement = window.document.body;
+		if (enableDarkMode) {
+			bodyElement.classList.add(className);
+		} else {
+			bodyElement.classList.remove(className);
+		}
+	}, [enableDarkMode]);
+	return [enableDarkMode, setEnableDarkMode];
 };
+export default useDarkMode;
