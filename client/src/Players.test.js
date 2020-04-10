b4a0278c4884de983ac4aeb742183e515a2751fb
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-10 12:55:22 -0500

    MVP met

--- client/src/Players.test.js
+++ client/src/Players.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { toBeInDocument } from '@testing-library/jest-dom/extend-expect';
 import Players from './Players';
 import App from './App';
@@ -13,12 +13,36 @@ test('Names appear', () => {
 	const nameText = getByText(/names:/i);
 	expect(nameText).toBeDefined();
 });
+
+test('header to  appear', () => {
+	const { getByText } = render(
+		<App>
+			<Players />
+		</App>
+	);
+	const headerText = getByText(/women's football players/i);
+	expect(headerText).toBeDefined();
+});
 // test('Player header is appearing', () => {
-// 	const { getByText } = render(<Players />);
-// 	getByText(/alex/i);
+// 	const { getByText } = render(
+// 		<App>
+// 			<Players />
+// 		</App>
+// 	);
+
+// 	expect(
+// 		queryByTestId(document.documentElement, 'html-element')
+// 	).toBeInTheDocument();
+// 	// const headerText = getByText(/Women's football players/i);
+// 	// expect(headerText).toBeVisable();
 // });
-// test('Players  nameis appearing', () => {
-// 	const { getByText } = render(<Players />);
-// 	const alexText = getByText('Alex');
-// 	expect(alexText).toBeInDocument();
+// test('Header is appearing', () => {
+// 	const { getByText } = render(
+// 		<App>
+// 			<Players />
+// 		</App>
+// 	);
+// 	const alexText = getByText(/Women's football players/i);
+// 	// expect(alexText).toBeVisable();
+// 	expect(getByText(alexText)).not.toBeVisible();
 // });
