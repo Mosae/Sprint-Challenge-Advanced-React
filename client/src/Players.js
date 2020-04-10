7ce94ab237057aa66d4f718e636ef4c066c27e74
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-10 10:56:39 -0500

    Created a players component - names displaying

--- client/src/Players.js
+++ client/src/Players.js
@@ -4,11 +4,12 @@ const Players = (props) => {
 	return (
 		<div>
 			<h1>Players</h1>
+			<hr />
 			<h3>
 				Names:
-				{props.players.map((el) => {
-					console.log(el);
-				})}
+				{props.players.map((el) => (
+					<p>{el.name}</p>
+				))}
 			</h3>
 		</div>
 	);
